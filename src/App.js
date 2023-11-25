7400c7cf51d19958551020862daafeb1c5d8d001
unknown <Anirudha7221@example.com>
2023-11-25 17:12:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import SearchBar from './Component/SearchBar';
+
+const App = ()=>{
+
+ return(
+    <div className="App">
+       <SearchBar></SearchBar>
+    </div>
+ )
+}
+
+export default App;
